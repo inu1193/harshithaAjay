9cff679744fe2f280e2983577291156d0afce1bb
Yathish Shettigar <142146642+yathish1204@users.noreply.github.com>
2024-10-05 01:03:09 +0530

    Updated Hero Muhurtham and venue

--- app/page.js
+++ app/page.js
@@ -1,9 +1,18 @@
+import Button from '@/components/Button/Button'
+import Hero from '@/components/Hero/Hero'
+import Muhurtham from '@/components/Muhurtham/Muhurtham'
+import Venue from '@/components/Venue/Venue'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      Rest everything goes here
+      <Hero />
+      <Muhurtham />
+      <Button />
+      <Venue/>
+
+      {/* Rest everything goes here */}
       {/* Make everything as a seperate components as I did for Navbar and Footer and work on it and Then place it over here in a order */}
       {/* If there is a common css means write that in a global.css file */}
     </div>
---
+++ components/Venue/Venue.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from './venue.module.css'
+
+const Venue = () => {
+  return (
+    <main className='main'>
+      <div className={styles.venueSec}>
+            <h1>Venue</h1>
+            <div>
+                <h2>RATHNA CONVENTION CENTRE</h2>
+                <p>KOLAR-MULABAGILU ROAD, KOLAR</p>
+            </div>
+      </div>
+    </main>
+  )
+}
+
+export default Venue
